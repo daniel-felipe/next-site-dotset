fdc98f05d74064b8e2406b182fcff74e643b83f1
= <daniell.felipe.2017@gmail.com>
2025-05-31 10:06:34 -0300

    Create call-to-action section

---
+++ src/components/call-to-action/index.tsx
@@ -0,0 +1,34 @@
+import { ArrowRight, Store } from 'lucide-react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { PT_Sans_Caption } from 'next/font/google'
+
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ['latin'],
+  weight: '700',
+})
+
+export function CallToAction() {
+  return (
+    <section className="flex flex-col justify-center items-center bg-[url('/call-to-action-background.svg')] bg-cover bg-top">
+      <div className="p-4 bg-cyan-300 w-fit rounded-full -mt-7">
+        <Store className="text-cyan-100" />
+      </div>
+
+      <div className="container py-20 flex flex-col items-center justify-center gap-10 text-center">
+        <h2
+          className={`${ptSansCaption.className} text-gray-100 text-balance text-heading-xl max-w-lg`}
+        >
+          Crie uma loja online e inicie seus vendas ainda hoje
+        </h2>
+
+        <Button variant="primary" asChild>
+          <Link href="/criar-loja">
+            Criar loja grátis
+            <ArrowRight />
+          </Link>
+        </Button>
+      </div>
+    </section>
+  )
+}
