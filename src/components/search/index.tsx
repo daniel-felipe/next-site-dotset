017e9079bd96d094b7c69c878c867cdca5329428
= <daniell.felipe.2017@gmail.com>
2025-06-07 16:16:29 -0300

    Add search input reset button

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,11 +1,11 @@
 import { cn } from '@/lib/utils'
-import { SearchIcon } from 'lucide-react'
+import { CircleX, SearchIcon } from 'lucide-react'
 import { useRouter } from 'next/router'
 import { useCallback } from 'react'
 
 export function Search() {
   const router = useRouter()
-  const query = router.query.q as string
+  const query = (router.query.q as string) ?? ''
 
   const handleSearch = useCallback(
     (event: React.FormEvent) => {
@@ -27,20 +27,36 @@ export function Search() {
     })
   }
 
+  function handleResetSearch() {
+    router.push('/blog', undefined, {
+      shallow: true,
+      scroll: false,
+    })
+  }
+
   return (
-    <form onSubmit={handleSearch} className="relative group">
+    <form onSubmit={handleSearch} className="relative group w-full md:max-w-60">
       <SearchIcon
         className={cn(
           'text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 size-4 transition-colors duration-200 group-focus-within:text-blue-300',
           query ? 'text-blue-300' : ''
         )}
       />
+
       <input
         type="text"
         placeholder="Buscar"
-        className="h-10 w-72 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-withing:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
+        value={query}
+        className="w-full h-10 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-withing:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm md:w-60"
         onChange={handleQueryChange}
       />
+
+      {query && (
+        <CircleX
+          className="absolute size-4 right-3 top-1/2 -translate-y-1/2 text-gray-300"
+          onClick={handleResetSearch}
+        />
+      )}
     </form>
   )
 }
