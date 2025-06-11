ebf2e72959ef4d8ba0c00432f8ed729fffd1906d
= <daniell.felipe.2017@gmail.com>
2025-06-10 21:32:04 -0300

    implement post search

--- src/templates/blog/index.tsx
+++ src/templates/blog/index.tsx
@@ -1,17 +1,27 @@
-import { Search } from '@/components/search'
 import { useRouter } from 'next/router'
+import { allPosts, Post } from 'contentlayer/generated'
+
+import { Search } from '@/components/search'
+
 import { PostCard } from './components/post-card'
 import { PostList } from './components/post-list'
-import { allPosts } from 'contentlayer/generated'
+import { Inbox } from 'lucide-react'
 
 export function BlogList() {
   const router = useRouter()
   const query = router.query.q as string
+
   const pageTitle = query
     ? `Resultados de busca para "${query}"`
     : 'Dicas e estratégias para impulsionar seu negócio'
 
-  const posts = allPosts
+  const posts = query
+    ? allPosts.filter((post) =>
+        post.title.toLowerCase()?.includes(query.trim().toLowerCase())
+      )
+    : allPosts
+
+  const hasPosts = posts.length > 0
 
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
@@ -31,22 +41,31 @@ export function BlogList() {
         </div>
       </header>
 
-      <PostList>
-        {posts.map((post) => (
-          <PostCard
-            key={post._id}
-            title={post.title}
-            description={post.description}
-            date={new Date(post.date).toLocaleDateString('pt-BR')}
-            slug={post.slug}
-            image={post.image}
-            author={{
-              name: post.author.name,
-              avatar: post.author.avatar,
-            }}
-          />
-        ))}
-      </PostList>
+      {hasPosts ? (
+        <PostList>
+          {posts.map((post) => (
+            <PostCard
+              key={post._id}
+              title={post.title}
+              description={post.description}
+              date={new Date(post.date).toLocaleDateString('pt-BR')}
+              slug={post.slug}
+              image={post.image}
+              author={{
+                name: post.author.name,
+                avatar: post.author.avatar,
+              }}
+            />
+          ))}
+        </PostList>
+      ) : (
+        <div className="container px-8">
+          <div className="flex flex-col items-center justify-center gap-8 rounded-lg border-dashed border-2 border-gray-300 p-8 md:p-12">
+            <Inbox className="size-12 text-cyan-100" />
+            <p className="text-gray-100 text-center">Nenhum post encontrado</p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
