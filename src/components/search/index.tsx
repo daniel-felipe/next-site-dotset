474441998c55f810731ab2a3a33442fb64418647
= <daniell.felipe.2017@gmail.com>
2025-07-08 21:17:21 -0300

    add NotFound page and enhance Search component with focus behavior

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,12 +1,15 @@
 import { cn } from '@/lib/utils'
 import { CircleX, SearchIcon } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 
 export function Search() {
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
   const router = useRouter()
   const searchParams = useSearchParams()
   const query = searchParams?.get('q') ?? ''
+  const hasQuery = searchParams?.has('q')
 
   const handleSearch = useCallback(
     (event: React.FormEvent) => {
@@ -29,6 +32,12 @@ export function Search() {
     router.push('/blog', { scroll: false })
   }
 
+  useEffect(() => {
+    if (hasQuery) {
+      inputRef.current?.focus()
+    }
+  }, [hasQuery])
+
   return (
     <form onSubmit={handleSearch} className="relative group w-full md:max-w-60">
       <SearchIcon
@@ -39,6 +48,7 @@ export function Search() {
       />
 
       <input
+        ref={inputRef}
         type="text"
         placeholder="Buscar"
         value={query}
