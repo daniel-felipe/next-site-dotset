739e1063feef39771a40739830c27acea7e7c0bf
= <daniell.felipe.2017@gmail.com>
2025-07-12 13:43:35 -0300

    Add metadata generation for blog posts and enhance layout Open Graph settings

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { allPosts } from 'contentlayer/generated'
+import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
 import { PostPage } from '@/templates/blog/post-page'
@@ -8,6 +9,29 @@ type BlogPostPageProps = {
     slug: string
   }>
 }
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params
+  const post = allPosts.find((post) => post.slug === slug)
+
+  if (!post) {
+    return {}
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: 'index, follow',
+    openGraph: {
+      images: [post.image],
+    },
+  }
+}
+
+export const revalidate = 60
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,22 @@ import '@/styles/globals.css'
 export const metadata: Metadata = {
   title: 'Site.Set',
   description: 'Venda seus produtos como afiliado em um único lugar',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: 'https://next-site-dotset.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://next-site-dotset.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
